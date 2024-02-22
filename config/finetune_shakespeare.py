dea699817efcd668e38e22681ed788ce9c1ce108
last-brain-cell <naadkd@gmail.com>
2024-02-23 03:03:11 +0530

    ok

--- config/finetune_shakespeare.py
+++ config/finetune_shakespeare.py
@@ -2,7 +2,7 @@ import time
 
 out_dir = 'out-shakespeare'
 eval_interval = 5
-eval_iters = 40
+eval_iters = 10
 wandb_log = False # feel free to turn on
 wandb_project = 'shakespeare'
 wandb_run_name = 'ft-' + str(time.time())
