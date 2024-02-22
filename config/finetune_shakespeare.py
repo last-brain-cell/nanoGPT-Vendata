0c2a881676142e4cfe146d56d80d1fef0e59fa8c
last-brain-cell <naadkd@gmail.com>
2024-02-23 02:46:14 +0530

    ok

--- config/finetune_shakespeare.py
+++ config/finetune_shakespeare.py
@@ -11,14 +11,14 @@ dataset = 'shakespeare'
 init_from = 'gpt2' # this is the largest GPT-2 model
 
 # only save checkpoints if the validation loss improves
-always_save_checkpoint = False
+always_save_checkpoint = True
 
 # the number of examples per iter:
 # 1 batch_size * 32 grad_accum * 1024 tokens = 32,768 tokens/iter
 # shakespeare has 301,966 tokens, so 1 epoch ~= 9.2 iters
 batch_size = 8
 gradient_accumulation_steps = 32
-max_iters = 50
+max_iters = 30
 
 # finetune at constant LR
 learning_rate = 3e-5
